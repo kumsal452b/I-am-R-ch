bf447c8a9c8bb2671e9f6378f0681c4592c8a737
Yahya Alataş <yahya.alatas.mdbf17@iste.edu.tr>
2023-08-03 22:02:46 +0300

    Initial Commit

---
+++ I am RıchUITests/I_am_R_chUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_am_R_chUITestsLaunchTests.swift
+//  I am RıchUITests
+//
+//  Created by a on 3.08.2023.
+//
+
+import XCTest
+
+final class I_am_R_chUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
